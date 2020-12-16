217eb23120980cb80affbea0aa11b979d87a3f96
Marco Antoniotti <marcoxa@gmail.com>
2020-12-16 22:21:15 +0100

    Expanded the description.

--- iron-main-pkg.el
+++ iron-main-pkg.el
@@ -23,8 +23,10 @@
 (define-package "IRON MAIN" "20201206.1"
   "The Iron Main package.
 
-The Iron Main package is a collection of modes and code to allow Emacs
-to edit and interface (minimally) with a IBM MVS or z/OS instance."
+The Iron Main package is a collection of modes and code to allow
+Emacs to edit and interface (minimally) with a IBM MVS or z/OS
+instance; mostly assuming that they are on the free Hercules
+emulator."
   )
 
 
