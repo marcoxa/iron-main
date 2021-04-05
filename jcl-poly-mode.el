db6ebc8235b40c034e98199970a82d081791619d
Marco Antoniotti <marcoxa@gmail.com>
2021-04-05 13:02:12 +0200

    Added file.  JCL poly mode.

---
+++ jcl-poly-mode.el
@@ -0,0 +1,94 @@
+;;; jcl-poly-mode --- A major polymode mode to handle MVS or Z/OS JCL.
+;;; -*- Mode: Emacs-Lisp; lexical-binding: t; -*-
+
+;;; jcl-poly-mode.el
+;;
+;; See the file COPYING license and copyright information.
+;;
+;; Author: Marco Antoniotti <marcoxa [at] gmail.com>
+;;
+;; Created: April, 4th, 2021
+;;
+;; Version: 20210405.1
+;;
+;; Keywords: languages, operating systems.
+
+;;; Commentary:
+;;
+;; A major polymode mode to handle IBM MVS or z/OS JCL.
+;;
+;; For the time being only the following languages are supported.
+;; * PL/I
+;; * Fortran (H)
+;; * COBOL
+;; * Assembler
+;; * C (JCC and GCC)
+;;
+;; The list is taken from TK4- SYS2.JCLLIB listing. The list is
+;; missing Simula and Pascal.  In the future Jay Moseley's language
+;; pack may be fully supported, provided that proper modes are
+;; available in Emacs for those languages.
+
+;;; Code:
+
+(require 'polymode)
+
+;;;; JCL Poly Mode Setup.
+
+(use-package polymode
+  :ensure t
+  :mode ("\\.jcl$" . jcl-poly-mode)
+  :config
+
+  (define-hostmode jcl-poly-hostmode :mode 'jcl-mode)
+
+  (define-innermode jcl-pl1-innermode
+    :mode 'pl1-mode
+    :head-matcher "^//PL1L\\.SYSIN +DD +\\*"
+    :tail-matcher "^\\(/\\*\\|//\\)"
+    :head-mode 'host
+    :tail-mode 'host)
+
+  (define-innermode jcl-fortran-innermode
+    :mode 'fortran-mode
+    :head-matcher "^//FORT\\.SYSIN +DD +\\*"
+    :tail-matcher "^\\(/\\*\\|//\\)"
+    :head-mode 'host
+    :tail-mode 'host)
+
+  (define-innermode jcl-cobol-innermode
+    :mode 'cobol-mode
+    :head-matcher "^//COB\\.SYSIN +DD +\\*"
+    :tail-matcher "^\\(/\\*\\|//\\)"
+    :head-mode 'host
+    :tail-mode 'host)
+
+  (define-innermode jcl-asmibm-innermode
+    :mode 'asmibm-mode
+    :head-matcher "^//ASM\\.SYSIN +DD +\\*"
+    :tail-matcher "^\\(/\\*\\|//\\)"
+    :head-mode 'host
+    :tail-mode 'host)
+
+  (define-innermode jcl-c-innermode
+    :mode 'c-mode
+    ;; The next should accomodate both JCC and GCC.
+    :head-matcher "^//COMP\\(ILE\))\\.SYSIN +DD +\\*"
+    :tail-matcher "^\\(/\\*\\|//\\)"
+    :head-mode 'host
+    :tail-mode 'host)
+
+  (define-polymode jcl-poly-mode
+    :hostmode 'jcl-poly-hostmode
+    :innermodes '(jcl-pl1-innermode
+		  jcl-fortran-innermode
+		  jcl-cobol-innermode
+		  jcl-asmibm-innermode)
+    ))
+
+
+;;; Epilogue.
+
+(provide 'jcl-poly-mode)
+
+;;; jcl-poly-mode.el ends here
