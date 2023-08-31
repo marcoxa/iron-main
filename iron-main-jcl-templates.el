97db4eeb56ccaa1a8a7ce5140689c035a4baccfd
Marco Antoniotti <marcoxa@gmail.com>
2023-08-31 10:54:05 +0200

    Checkpoint...  First draft.

--- iron-main-jcl-templates.el
+++ iron-main-jcl-templates.el
@@ -9,18 +9,19 @@
 ;;
 ;; Created: December 17th, 2020.
 ;;
-;; Version: 20201206.1
+;; Version: 20230830.1
 ;;
 ;; Keywords: languages, operating systems.
 
 ;;; Commentary:
 ;;
 ;; JCL templates used by `iron-main-mode'.
-;; A minor mode to edit files and interact with IBM MVS or z/OS.
 ;;
 ;; The following are functions and data structures to handle the
 ;; simple JCL templates used to allocate and save files in datasets the
 ;; mainframe.
+;;
+;; Functions and variables are prefixed with "iron-main-jcl-tmpl--".
 ;;
 ;; To Do:
 ;;
@@ -30,14 +31,17 @@
 
 ;;;; IRON MAIN Mode JCL Templates.
 
+
 (require 'cl-lib)
 
 ;; Allocation template.
 
-(defvar iron-main-jcl-job-template
+(defvar iron-main-jcl-tmpl--job
   "//* -*- Mode: JCL -*-
-//* IRON MAIN Emacs generated job.
-//EMJ%d    JOB (EMACS),'%s'
+//* IRON MAIN Emacs generated job - %s.
+//* %s
+//*
+//EMIM%d   JOB (EMACS),'%s'
 //            CLASS=A,
 //            MSGCLASS=%s,
 //            MSGLEVEL=(1,1)
@@ -46,18 +50,27 @@
   "Template for Emacs Job."
   )
 
-(defun iron-main-jcl-job (job-number programmer &optional msgclass)
+(cl-defun iron-main-jcl-tmpl--make-job (&key
+					 (job-number 42)
+					 (programmer "MYSELF")
+					 (msgclass "H")
+					 (header-line "")
+					 )
+						    
   "Generate the Emacs Job Header.
 
 The values for JOB-NUMBER and PROGRAMMER are spliced in as is the
 MSGCLASS (this is optional and defaults to 'H'."
-  (unless msgclass
-    (setq msgclass "H"))
   
-  (format iron-main-jcl-job-template job-number programmer msgclass))
+  (format iron-main-jcl-tmpl--job
+	  (current-time-string)
+	  header-line
+	  job-number
+	  programmer
+	  msgclass))
 
 
-(defvar iron-main-jcl-exist-step-template
+(defvar iron-main-jcl-tmpl--exist-step
   "//* IRON MAIN Emacs generated step.
 //* Check if dataset exists.
 //*
@@ -71,20 +84,20 @@ MSGCLASS (this is optional and defaults to 'H'."
   )
 
 
-(defun iron-main-jcl-exists-step (ds-rep)
-  "Generate the Emacs 'exists'step given a dataset rep DS-REP."
-  (format iron-main-jcl-exist-step-template
+(cl-defun iron-main-jcl-tmpl--make-exists-step (ds-rep)
+  "Generate the Emacs `exists' step given a dataset rep DS-REP."
+  (format iron-main-jcl-tmpl--exist-step
 	  (string-trim (iron-main-ds-rep-name ds-rep) "'" "'")))
   
 
 
-(defvar iron-main-jcl-allocate-step-template
+(defvar iron-main-jcl-tmpl--allocate-step
   "//* IRON MAIN Emacs generated step.
 //* Create dataset.
 //*
 //EMDSCREA EXEC PGM=IEFBR14%s
 //SYSPRINT   DD SYSOUT=*
-//NEW        DD DSN=%s,DISP=(NEW,CATLG),
+//NEWDS      DD DSN=%s,DISP=(NEW,CATLG),
 //           LRECL=%d,RECFM=%s,
 //           SPACE=(%s,(%d,%d,%d)),
 //           DSORG=%s
@@ -93,8 +106,9 @@ MSGCLASS (this is optional and defaults to 'H'."
   )
 
 
-(defun iron-main-jcl-allocate-step (ds-rep &optional cond)
-  "Generate the Emacs 'allocation' step given a dataset rep DS-REP.
+
+(defun iron-main-jcl-tmpl--make-allocate-step (ds-rep &optional cond)
+  "Generate the Emacs `allocation' step given a dataset rep DS-REP.
 
 The optional COND defaults to the empty string, otherwise it should be
 a condition that could prevent the step from running, possibly
@@ -103,7 +117,7 @@ referring to previous steps."
   (unless cond
     (setq cond ""))
   
-  (format iron-main-jcl-allocate-step-template
+  (format iron-main-jcl-tmpl--allocate-step
 	  cond
 	  (iron-main-ds-rep-name ds-rep)
 	  (iron-main-ds-rep-lrecl ds-rep)
@@ -116,20 +130,20 @@ referring to previous steps."
 	  ))
 
 
-(defvar iron-main-jcl-delete-step-template
+(defvar iron-main-jcl-tmpl--delete-step
   "//* IRON MAIN Emacs generated step.
 //* Delete dataset.
 //*
 //EMDSDEL EXEC PGM=IEFBR14%s
 //SYSPRINT  DD SYSOUT=*
-//DEL       DD DSN=%s,DISP=(OLD,DELETE,DELETE)
+//DELDS     DD DSN=%s,DISP=(OLD,DELETE,DELETE)
 "
   "Template for Emacs dataset deletion job step."
   )
 
 
 (defun iron-main-jcl-delete-step (ds-rep &optional cond)
-  "Generate the Emacs 'delet' step given a dataset rep DS-REP.
+  "Generate the Emacs `delete' step given a dataset rep DS-REP.
 
 The optional COND defaults to the empty string, otherwise it should be
 a condition that could prevent the step from running, possibly
@@ -138,10 +152,61 @@ referring to previous steps."
   (unless cond
     (setq cond ""))
   
-  (format iron-main-jcl-delete-step-template
+  (format iron-main-jcl-tmpl--delete-step
 	  cond
 	  (iron-main-ds-rep-name ds-rep)
 	  ))
+
+
+;; Buffer generation.
+
+;; Just temporary...
+
+(cl-defun iron-main-jcl-tmpl--instantiate (buffer-or-name
+					   jcl-template-fun
+					   &rest
+					   args)
+  
+  (switch-to-buffer buffer-or-name)
+
+  (kill-all-local-variables)
+
+  (erase-buffer)
+
+  (insert
+   (apply jcl-template-fun args))
+
+  (jcl-poly-mode)
+  )
+
+
+;; Allocation JCL
+
+(cl-defun iron-main-jcl-tmpl--allocation-job
+    (buffer-or-name
+     &aux
+     (dsrep
+      (make-iron-main-ds-rep :name "FOOO.BAR.BAZ")
+      ))
+    
+  (switch-to-buffer buffer-or-name)
+
+  (kill-all-local-variables)
+
+  (erase-buffer)
+
+  (insert
+   (iron-main-jcl-tmpl--make-job
+    :header-line "Dataset Allocation Job.")
+   (iron-main-jcl-tmpl--make-exists-step dsrep)
+   (iron-main-jcl-tmpl--make-allocate-step dsrep ",COND(8)")
+   "//\n"
+   "//* End IRON MAIN Emacs generated job.\n"
+   )
+
+  (jcl-poly-mode)
+  )
+
 
 
 ;;;; Epilogue
