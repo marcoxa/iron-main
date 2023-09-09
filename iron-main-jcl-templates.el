91a0dcc24c7dc717debb6a21887583d0bee83fc7
Marco Antoniotti <marcoxa@gmail.com>
2023-09-09 19:26:41 +0200

    Added JCL generation code.

--- iron-main-jcl-templates.el
+++ iron-main-jcl-templates.el
@@ -33,6 +33,7 @@
 
 
 (require 'cl-lib)
+(require 'iron-main-session)
 
 ;; Allocation template.
 
@@ -44,7 +45,9 @@
 //EMIM%d   JOB (EMACS),'%s'
 //            CLASS=A,
 //            MSGCLASS=%s,
-//            MSGLEVEL=(1,1)
+//            MSGLEVEL=(1,1),
+//            USER='%s',
+//            PASSWORD='%s'
 //*
 "
   "Template for Emacs Job."
@@ -53,6 +56,8 @@
 (cl-defun iron-main-jcl-tmpl--make-job (&key
 					 (job-number 42)
 					 (programmer "MYSELF")
+					 (user "LUSER")
+					 (password "PASSWORD")
 					 (msgclass "H")
 					 (header-line "")
 					 )
@@ -67,7 +72,10 @@ MSGCLASS (this is optional and defaults to 'H'."
 	  header-line
 	  job-number
 	  programmer
-	  msgclass))
+	  msgclass
+	  user
+	  password
+	  ))
 
 
 (defvar iron-main-jcl-tmpl--exist-step
@@ -75,7 +83,7 @@ MSGCLASS (this is optional and defaults to 'H'."
 //* Check if dataset exists.
 //*
 //EXISTCHK EXEC PGM=IEFBR14
-//EXISTDSN      DD DSN='%s',
+//EXISTDSN   DD DSN='%s',
 //              DISP=(OLD,KEEP)
 //* CC should be non zero if DSN does not exist.
 //*
@@ -87,7 +95,8 @@ MSGCLASS (this is optional and defaults to 'H'."
 (cl-defun iron-main-jcl-tmpl--make-exists-step (ds-rep)
   "Generate the Emacs `exists' step given a dataset rep DS-REP."
   (format iron-main-jcl-tmpl--exist-step
-	  (string-trim (iron-main-ds-rep-name ds-rep) "'" "'")))
+	  (upcase
+	   (string-trim (iron-main-ds-rep-name ds-rep) "'" "'"))))
   
 
 
@@ -97,10 +106,12 @@ MSGCLASS (this is optional and defaults to 'H'."
 //*
 //EMDSCREA EXEC PGM=IEFBR14%s
 //SYSPRINT   DD SYSOUT=*
-//NEWDS      DD DSN=%s,DISP=(NEW,CATLG),
-//           LRECL=%d,RECFM=%s,
-//           SPACE=(%s,(%d,%d,%d)),
-//           DSORG=%s
+//NEWDS      DD DSN='%s',DISP=(NEW,CATLG),
+//              LRECL=%d,RECFM=%s,
+//              SPACE=(%s,(%d,%d,%d)),
+//              DSORG=%s,
+//              VOL=%s,
+//              UNIT=%s
 "
   "Template for Emacs dataset allocation job step."
   )
@@ -114,18 +125,39 @@ MSGCLASS (this is optional and defaults to 'H'."
 The optional COND defaults to the empty string, otherwise it should be
 a condition that could prevent the step from running, possibly
 referring to previous steps."
+
+  (let ((dsname (upcase (iron-main-ds-rep-name ds-rep)))
+	(lrecl (iron-main-ds-rep-lrecl ds-rep))
+	(recfm (iron-main-ds-rep-recfm ds-rep))
+	(space-unit (iron-main-ds-rep-space-unit ds-rep))
+	(primary (iron-main-ds-rep-primary ds-rep))
+	(secondary (iron-main-ds-rep-secondary ds-rep))
+	(directory (iron-main-ds-rep-directory ds-rep))
+	(dsorg (iron-main-ds-rep-dsorg ds-rep))
+	(vol (iron-main-ds-rep-vol ds-rep))
+	(unit (iron-main-ds-rep-unit ds-rep))
+	)
+
+    
+    (when (string= "" dsname)
+      (error "IMJT1E: empty dsname in allocation step"))
+    
+    (when (string= "" vol)
+      (error "IMJT2E: empty volume in allocation step"))
   
-  (format iron-main-jcl-tmpl--allocate-step
-	  cond
-	  (iron-main-ds-rep-name ds-rep)
-	  (iron-main-ds-rep-lrecl ds-rep)
-	  (iron-main-ds-rep-recfm ds-rep)
-	  (iron-main-ds-rep-space-unit ds-rep)
-	  (iron-main-ds-rep-primary ds-rep)
-	  (iron-main-ds-rep-secondary ds-rep)
-	  (iron-main-ds-rep-directory ds-rep)
-	  (iron-main-ds-rep-dsorg ds-rep)
-	  ))
+    (format iron-main-jcl-tmpl--allocate-step
+	    cond
+	    dsname
+	    lrecl
+	    recfm
+	    space-unit
+	    primary
+	    secondary
+	    directory
+	    dsorg
+	    vol
+	    unit
+	    )))
 
 
 (defvar iron-main-jcl-tmpl--delete-step
@@ -175,15 +207,11 @@ referring to previous steps."
   )
 
 
-;; Allocation JCL
-
-(cl-defun iron-main-jcl-tmpl--allocation-job
-    (buffer-or-name
-     &aux
-     (dsrep
-      (make-iron-main-ds-rep :name "FOOO.BAR.BAZ")
-      ))
-    
+(cl-defun iron-main-jcl-tmpl--show (buffer-or-name
+				    jcl-template-fun
+				    &rest
+				    args)
+  
   (switch-to-buffer buffer-or-name)
 
   (kill-all-local-variables)
@@ -191,15 +219,112 @@ referring to previous steps."
   (erase-buffer)
 
   (insert
-   (iron-main-jcl-tmpl--make-job
-    :header-line "Dataset Allocation Job.")
-   (iron-main-jcl-tmpl--make-exists-step dsrep)
-   (iron-main-jcl-tmpl--make-allocate-step dsrep ",COND(8)")
-   "//\n"
-   "//* End IRON MAIN Emacs generated job.\n"
-   )
+   (apply jcl-template-fun args))
 
   (jcl-poly-mode)
+  )
+
+
+;; Allocation JCL
+;;
+;; Notes:
+;; I could, actually should, be smarter about reusing the buffer (and
+;; getting rid of it).
+
+(cl-defun iron-main-jcl-tmpl--make-alloc-job-buffer
+    (&key
+     (user "LUSER")
+     (password "PASSWORD")
+     (dsrep
+      (make-iron-main-ds-rep :name "FOOO.BAR.BAZ")
+      )
+     &aux
+     (alloc-job-buffer
+      (get-buffer-create
+       (format "*IRON MAIN Alloc Job for %s"
+	       (iron-main-ds-rep-name dsrep))))
+     )
+
+  (with-current-buffer alloc-job-buffer
+    (kill-all-local-variables)
+
+    (erase-buffer)
+
+    (insert
+     (iron-main-jcl-tmpl--make-job
+      :user user
+      :password password
+      :header-line "Dataset Allocation Job.")
+     (iron-main-jcl-tmpl--make-exists-step dsrep)
+     (iron-main-jcl-tmpl--make-allocate-step dsrep
+					     ",COND(NE,0,EXISTCHK)")
+     "//\n"
+     "//* End IRON MAIN Emacs generated job.\n"
+     )
+
+    (jcl-poly-mode)
+    )
+  alloc-job-buffer
+  )
+
+
+(cl-defun iron-main-jcl-tmpl--allocation-job
+    (session
+     &key
+     (dsrep
+      (make-iron-main-ds-rep :name "FOOO.BAR.BAZ"))
+     (user (iron-main-session-user session))
+     (pw (iron-main-session-passwd session))
+     
+     &aux
+     (dsname (iron-main-ds-rep-name dsrep))
+     (alloc-job-buffer nil)
+     )
+
+  (when (string= "" dsname)
+    (error "IMJT01E: empty dataset name for allocation job."))
+    
+  (setq alloc-job-buffer
+	(iron-main-jcl-tmpl--make-alloc-job-buffer
+	 :user user
+	 :password pw
+	 :dsrep dsrep))
+
+  (if alloc-job-buffer
+      (switch-to-buffer alloc-job-buffer)
+    (error "IMJT02E: could not create buffer for allocation job."))
+  )
+
+
+(cl-defun iron-main-jcl-tmpl--run-allocation-job
+    (session
+     &key
+     (dsrep
+      (make-iron-main-ds-rep :name "FOOO.BAR.BAZ"))
+     (user (iron-main-session-user session))
+     (pw (iron-main-session-passwd session))
+     
+     &aux
+     (dsname (iron-main-ds-rep-name dsrep))
+     (alloc-job-buffer nil)
+     )
+
+  (when (string= "" dsname)
+    (error "IMJT01E: empty dataset name for allocation job."))
+    
+  (setq alloc-job-buffer
+	(iron-main-jcl-tmpl--make-alloc-job-buffer
+	 :user user
+	 :password pw
+	 :dsrep dsrep))
+
+  (if alloc-job-buffer
+      (progn
+	(message "IMJT01I: we should run %s, but let's just show it."
+		 alloc-job-buffer)
+	(switch-to-buffer alloc-job-buffer)
+	)
+    (error "IMJT02E: could not create buffer for allocation job."))
   )
 
 
